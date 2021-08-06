020f5a88375709a6111228a50bea4013843107d4
Alex <35180934+Artnoc1@users.noreply.github.com>
2021-08-07 00:46:04 +0300

    update ssh-apt.sh

--- Scripts/SSH/Apt/ssh-apt.sh
+++ Scripts/SSH/Apt/ssh-apt.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#Get the necessary components
+#GET_THE_NECESSARY_COMPONENTS
 apt-get update
 apt-get install openssh-server -y
 
-#Setup the necessary files
+#SETUP_THE_NECESSARY_FILES
 wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/SSH/Apt/sshd_config -P /etc/ssh
 
 echo "You can now start OpenSSH Server by running /etc/init.d/ssh start"
